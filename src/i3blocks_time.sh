8f44970316ddf70b1d644229172d9b07015d37a8
Ivan Gagis <igagis@gmail.com>
2020-06-29 18:23:41 +0300

    stuff

--- src/i3blocks_time.sh
+++ src/i3blocks_time.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# NOTE: to close calendar on focus lost (click outside) do the following config:
+#
+#	$ mkdir -p ~/.config/gsimplecal
+#       $ echo 'close_on_unfocus = 1' >> ~/.config/gsimplecal/config
+#
+
 date '+%Y-%m-%d %H:%M:%S'
 
 if [ "$BLOCK_BUTTON" == "1" ]; then
